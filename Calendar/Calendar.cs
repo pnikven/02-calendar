3a9adebdff90e1741d52bfb642e9666c3bd2a224
pnikven <pyshkinnv@gmail.com>
2014-11-24 18:21:21 +0500

    Реализован метод InitializeValuesMatrix (распределение дней по дням недели)

--- Calendar/Calendar.cs
+++ Calendar/Calendar.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 
 namespace Calendar
@@ -19,12 +20,30 @@ namespace Calendar
         public static int[][] InitializeValuesMatrix(DateTime date)
         {
             var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
-            var firstDayOfNextMonth = firstDayOfMonth.AddMonths(1);
-            var lastDayOfMonth = firstDayOfNextMonth.AddDays(-1);
-            var dayOfWeek = DayOfWeekToInt(firstDayOfMonth.DayOfWeek);
-            var weekNumber = GetWeekNumber(firstDayOfMonth.DayOfYear);
+            var monthDayCount = DateTime.DaysInMonth(date.Year, date.Month);            
 
-            throw new NotImplementedException();
+            var currentDayOfMonth = 1;
+            var dayOfWeek = DayOfWeekToInt(firstDayOfMonth.DayOfWeek);
+            var weekNumber = GetWeekNumber(firstDayOfMonth.DayOfYear);            
+
+            var result = new List<int[]>();
+            var weekDays = new int[ValuesMatrixWidth];
+            weekDays[0] = weekNumber;
+            while (currentDayOfMonth<=monthDayCount)
+            {
+                weekDays[dayOfWeek] = currentDayOfMonth;
+                currentDayOfMonth++;
+                dayOfWeek++;
+                if (dayOfWeek <= 7) continue;
+                result.Add(weekDays);
+                weekDays = new int[ValuesMatrixWidth];
+                weekNumber++;
+                weekDays[0] = weekNumber;
+                dayOfWeek = 1;
+            }
+            result.Add(weekDays);
+
+            return result.ToArray();
         }
 
         public static int GetWeekNumber(int dayOfYear)
