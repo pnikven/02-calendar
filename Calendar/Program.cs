493a2a8e9097f246066d700053bbec010e4fb456
pnikven <pyshkinnv@gmail.com>
2014-11-24 13:03:28 +0500

    Добавлено считывание параметра даты из аргументов командной строки в формате dd.mm.yyyy

--- Calendar/Program.cs
+++ Calendar/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Calendar
+﻿using System;
+
+namespace Calendar
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var date = GetFirstParameter(args);
+        }
 
+        private static string GetFirstParameter(string[] consoleArguments)
+        {
+            if (consoleArguments.Length != 0) return consoleArguments[0];
+            throw new Exception("Date in format dd.mm.yyyy must be provided");
         }
     }
 }
